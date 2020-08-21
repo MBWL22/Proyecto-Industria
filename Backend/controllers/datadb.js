1cab8b68afee1e4cbc939a6970d599534cf54d6d
ledys09 <alvaradoestefany51@yahoo.es>
2020-08-20 23:10:33 -0600

    [UPDATE] login

--- Backend/controllers/datadb.js
+++ Backend/controllers/datadb.js
@@ -33,35 +33,3 @@ exports.services = async (req, res) => {
 
 }
 
-//@desc     data de usuario servicio
-//@route    GET /api/datadb/id
-//@access   Private
-
-exports.services = async (req, res) => {
-    const category = req.params.category;
-    //res.send(category);
-    const servicios = await userService.find({"servicios.categoria": category},
-    {
-        primerNombre :1, 
-        segundoNombre: 1,
-        primerApellido: 1,
-        'servicios.nombreServicio' : 1,
-        'servicios.descripcionServicio': 1,
-        'servicios.categoria': 1,
-
-    });
-    
-    try {
-        if(servicios.length === 0){
-            return res.status(404).json({
-                success: false,
-                msg: "Data not found",
-                
-            })
-        }
-        res.send(servicios);
-    } catch (error) {
-        return res.status(500).send(error);
-    }
-
-}
